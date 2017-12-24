58df2c9e5cc58b944d05d6ba9b039574d88b9c65
Nam Chu Hoai <nambrot@googlemail.com>
2017-12-24 16:57:49 -0500

    Only render tooltip for maxHeightBlock

--- src/components/BlockchainTree.js
+++ src/components/BlockchainTree.js
@@ -6,6 +6,7 @@ import { contains, pluck, pipe } from "ramda";
 import NewBlock from "./NewBlock";
 import DetailBlock from "./DetailBlock";
 import { Tooltip, advanceTo, Dialog as WalkthroughDialog } from "./walkthrough";
+import { last } from "ramda";
 
 function generateNodeProps(longestChain) {
   return function({ node, path }) {
@@ -13,6 +14,11 @@ function generateNodeProps(longestChain) {
       this.addBlockFrom(node);
       advanceTo(3);
     };
+    const normalButton = (
+      <Button key="add" text="Add block from here" onClick={addBlock} />
+    );
+    const isMaxHeightBlock = last(longestChain).hash === node.hash;
+
     return {
       buttons: [
         <Button
@@ -20,21 +26,25 @@ function generateNodeProps(longestChain) {
           iconName="pt-icon-database"
           onClick={this.showBlock(node)}
         />,
-        <Tooltip
-          content={
-            <p style={{ maxWidth: "250px" }}>
-              Mining blocks means adding blocks to another parent block by
-              pointing to it in the block header. Unless someone else gives you
-              coins, mining is the only way for you to get coins, so let's start
-              here.
-            </p>
-          }
-          next={addBlock}
-          nextLabel="Start mining!"
-          step={2}
-        >
-          <Button key="add" text="Add block from here" onClick={addBlock} />
-        </Tooltip>
+        isMaxHeightBlock ? (
+          <Tooltip
+            content={
+              <p style={{ maxWidth: "250px" }}>
+                Mining blocks means adding blocks to another parent block by
+                pointing to it in the block header. Unless someone else gives
+                you coins, mining is the only way for you to get coins, so let's
+                start here.
+              </p>
+            }
+            next={addBlock}
+            nextLabel="Start mining!"
+            step={2}
+          >
+            {normalButton}
+          </Tooltip>
+        ) : (
+          normalButton
+        )
       ],
       node: {
         title: `Block ${node.hash.substr(0, 10)}`,
